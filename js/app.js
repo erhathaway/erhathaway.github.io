564a32209da6a2018a4e1dcd4d63da1bcffc77b6
Ethan Hathaway <erhathaway@gmail.com>
2016-04-17 15:12:02 -0400

    refactor name and links into controller

--- js/app.js
+++ js/app.js
@@ -1,6 +1,30 @@
 var app = angular.module('myApp', ['ngMaterial']);
+
 app.controller('MyController', function($scope, $mdSidenav) {
   $scope.openLeftMenu = function() {
     $mdSidenav('left').toggle();
   };
+});
+
+app.controller('myName', function($scope) {
+  $scope.firstName = "Ethan";
+  $scope.lastName = "Hathaway";
+});
+
+app.controller('myLinks', function($scope) {
+  var data = {
+              "linkedin": {
+                            "link": "https://www.linkedin.com/in/erhathaway",
+                            "icon": "Linkedin-icon.png"
+                           },
+              "twitter": {
+                            "link" : "https://twitter.com/erhathaway",
+                            "icon" : null
+                         },
+              "github": {
+                          "link": "https://github.com/erhathaway",
+                          "icon" : null
+                        }
+            };
+  $scope.linkNames = Object.keys(data);
 });
